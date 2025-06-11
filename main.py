8f83f8e90bbc16d8178fdb4154a06ba97417caa0
SnowJ078 <jacob.snow078@gmail.com>
2025-06-10 22:02:09 -0400

    06/10/2025 Added movement to player character

--- main.py
+++ main.py
@@ -20,9 +20,15 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+            
+        player1.update(dt)
+
         screen.fill("black")
         player1.draw(screen)
         pygame.display.flip()
+
+        #caps framerate to 60 FPS
         dt = (clock.tick(60)) / 1000
+
 if __name__=="__main__":
     main()
