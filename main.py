f80673f0f381830a64f0f082db7de44d79e82d93
SnowJ078 <jacob.snow078@gmail.com>
2025-06-10 21:43:37 -0400

    06/10/2025 Added player icon

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from constants import *
+from player import *
 
 def main():
     print("Starting Asteroids!")
@@ -10,11 +11,18 @@ def main():
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    clock = pygame.time.Clock()
+    dt = 0
+
+    player1 = player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        player1.draw(screen)
         pygame.display.flip()
+        dt = (clock.tick(60)) / 1000
 if __name__=="__main__":
     main()
